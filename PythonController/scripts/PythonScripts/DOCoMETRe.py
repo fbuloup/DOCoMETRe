dd8995cba579fcdf73ba8a8043244a1abbd0aeaf
frank buloup <frank.buloup@univ-amu.fr>
2020-09-17 15:05:35 +0200

    Python Engine continue (2) : load data from samples files
    Signed-off-by: frank buloup <frank.buloup@univ-amu.fr>

--- PythonController/scripts/PythonScripts/DOCoMETRe.py
+++ PythonController/scripts/PythonScripts/DOCoMETRe.py
@@ -1,5 +1,6 @@
-from py4j.clientserver import ClientServer, JavaParameters, PythonParameters
-import os
+from py4j.clientserver import ClientServer, JavaParameters, PythonParameters;
+import os;
+import numpy;
 
 class DOCoMETRe(object):
 
@@ -32,12 +33,19 @@ class DOCoMETRe(object):
 	def loadDataDocometreSAMPLES(self, loadName, dataFilesList, sessionsProperties):
 		self.gateway.jvm.System.out.println("In loadDataDocometreSAMPLES");
 		self.gateway.jvm.System.out.println(sessionsProperties)
+
 		prefix_QN = "_DATA_FILES_NAMES_PREFIX";
 		baseTrialsNumber_QN = "_BASE_TRIALS_NUMBER";
+
+		createdCategories = dict();
+		createdChannels = list();
+
 		dataFiles = dataFilesList.split(";");
 		nbDataFiles = len(dataFiles);
+
 		maximumSamples = sessionsProperties["MAXIMUM_SAMPLES"];
 		totalTrialsNumber = sessionsProperties["TOTAL_TRIALS_NUMBER"];
+
 		for n in range(0, nbDataFiles):
 			segments = dataFiles[n].split(os.path.sep);
 
@@ -65,10 +73,72 @@ class DOCoMETRe(object):
 			if sessionName + prefix_QN in sessionsProperties:
 				channelName = fileNameSegments[1];
 
+			if criteria in createdCategories:
+
+				append = False;
+				trialsList = createdCategories[criteria];
+
+				self.gateway.jvm.System.out.println(trialsList);
+
+				if isinstance(trialsList, list):
+					if int(trialNumber) not in trialsList:
+						append = True;
+				else:
+					if int(trialNumber) != trialsList:
+						append = True;
+
+				if append:
+					trialsList = numpy.append(trialsList, int(trialNumber));
+			else:
+
+				trialsList = int(trialNumber);
+
+			createdCategories[criteria] = trialsList;
+
+			if channelName not in createdChannels:
+				createdChannels.append(channelName);
+				sampleFrequency = sessionsProperties[channelName + "_SF"];
+				experiments[loadName + "." + channelName + "." + "SampleFrequency"] = sampleFrequency;
+				experiments[loadName + "." + channelName + "." + "isSignal"] = "1";
+				experiments[loadName + "." + channelName + "." + "isCategory"] = "0";
+				experiments[loadName + "." + channelName + "." + "isEvent"] = "0";
+				experiments[loadName + "." + channelName + "." + "nbFields"] = "0";
+				experiments[loadName + "." + channelName + "." + "nbMarkers"] = "0";
+				experiments[loadName + "." + channelName + "." + "Values"] = numpy.zeros((int(totalTrialsNumber), int(maximumSamples)));
+
+			# Read data
+			data = numpy.fromfile(dataFiles[n], dtype="float32");
+			if system == "Arduino UNO":
+				data = data[2::2];
+
+			sizeData = len(data);
+			#self.gateway.jvm.System.out.println("sizeData : " + str(sizeData));
+			key = loadName + "." + channelName + ".NbSamples." + trialNumber;
+			experiments[key] = sizeData;
+			key = loadName + "." + channelName + ".EndCut." + trialNumber;
+			experiments[key] = sizeData + 1;
+			key = loadName + "." + channelName + ".FrontCut." + trialNumber;
+			experiments[key] = 0;
+			values = experiments[loadName + "." + channelName + ".Values"];
+			#self.gateway.jvm.System.out.println("size values : " + str(len(values[int(trialNumber) - 1])));
+			values[int(trialNumber) - 1][0:sizeData] = data;
+
+		n = 1;
+		for criteria in createdCategories:
+			values = createdCategories[criteria];
+			values.sort();
+			experiments[loadName + ".Category" + str(n) + ".Criteria"] = criteria;
+			experiments[loadName + ".Category" + str(n) + ".TrialsList"] = values;
+			experiments[loadName + "." + channelName + "." + "isSignal"] = "0";
+			experiments[loadName + "." + channelName + "." + "isCategory"] = "1";
+			experiments[loadName + "." + channelName + "." + "isEvent"] = "0";
+			n = n + 1;
+
 	class Java:
 		implements = ["fr.univamu.ism.docometre.python.PythonEntryPoint"]
 
 if __name__ == "__main__":
+	experiments = dict();
 	gateway = ClientServer(java_parameters = JavaParameters(), python_parameters = PythonParameters());
 	#gateway = 10
 	docometre = DOCoMETRe(gateway);
