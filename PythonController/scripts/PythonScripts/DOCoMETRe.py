2cf69fb1742c947dc6be22ddb82d6ed5d3f9bf20
frank buloup <frank.buloup@univ-amu.fr>
2020-09-21 18:15:19 +0200

    Python Engine continue (3) : load data from samples files
    Signed-off-by: frank buloup <frank.buloup@univ-amu.fr>

--- PythonController/scripts/PythonScripts/DOCoMETRe.py
+++ PythonController/scripts/PythonScripts/DOCoMETRe.py
@@ -1,18 +1,25 @@
 from py4j.clientserver import ClientServer, JavaParameters, PythonParameters;
+import sys;
 import os;
 import numpy;
+import io;
+import re;
 
 class DOCoMETRe(object):
 
+	global experiments;
+	global jvmMode;
+
 	def __init__(self, gateway):
 	 	self.gateway = gateway;
-	 	gateway.jvm.System.out.println("In __init__ gateway");
+	 	if(jvmMode): gateway.jvm.System.out.println("In __init__ gateway");
 
 	def shutDownServer(self, object):
-		self.gateway.jvm.System.out.println("In shutdown server");
+		if(jvmMode): self.gateway.jvm.System.out.println("In shutdown server");
+		pass;
 
 	def toString(self):
-		self.gateway.jvm.System.out.println("In toString");
+		if(jvmMode): self.gateway.jvm.System.out.println("In toString");
 		return "This is DOCoMETRe Python Entry Point";
 
 	def loadData(self, dataType, loadName, dataFilesList, sessionsProperties):
@@ -22,7 +29,7 @@ class DOCoMETRe(object):
 			pass;
 
 	def loadDataDocometre(self, loadName, dataFilesList, sessionsProperties):
-		self.gateway.jvm.System.out.println("In loadDataDocometre");
+		if(jvmMode): self.gateway.jvm.System.out.println("In loadDataDocometre");
 		if ".sau" in dataFilesList:
 			pass;
 		elif ".samples" in dataFilesList:
@@ -31,8 +38,8 @@ class DOCoMETRe(object):
 			pass;
 
 	def loadDataDocometreSAMPLES(self, loadName, dataFilesList, sessionsProperties):
-		self.gateway.jvm.System.out.println("In loadDataDocometreSAMPLES");
-		self.gateway.jvm.System.out.println(sessionsProperties)
+		if(jvmMode): self.gateway.jvm.System.out.println("In loadDataDocometreSAMPLES");
+		# if(jvmMode): self.gateway.jvm.System.out.println(sessionsProperties)
 
 		prefix_QN = "_DATA_FILES_NAMES_PREFIX";
 		baseTrialsNumber_QN = "_BASE_TRIALS_NUMBER";
@@ -54,13 +61,13 @@ class DOCoMETRe(object):
 			trialName = segments[len(segments) - 2];
 			key = os.path.dirname(os.path.abspath(dataFiles[n]))
 			process = sessionsProperties[key + "_PROCESS"];
-			#self.gateway.jvm.System.out.println("nbDataFiles : " + str(nbDataFiles) + " fichier : " + str(n+1) + " sessionName : " + sessionName + " process : " + process + " - " + dataFiles[n]);
+			# if(jvmMode): self.gateway.jvm.System.out.println("nbDataFiles : " + str(nbDataFiles) + " fichier : " + str(n+1) + " sessionName : " + sessionName + " process : " + process + " - " + dataFiles[n]);
 			criteria = sessionName + "." + process;
 			if sessionName + prefix_QN in sessionsProperties:
 				criteria = sessionsProperties[sessionName + prefix_QN];
 				criteria = criteria + "." + sessionName + "." + process;
-			#self.gateway.jvm.System.out.println("nbDataFiles : " + str(nbDataFiles) + " fichier : " + str(n+1) + " critere : " + criteria);
-			self.gateway.jvm.System.out.println(str(n) + " - " + dataFiles[n]);
+			# if(jvmMode): self.gateway.jvm.System.out.println("nbDataFiles : " + str(nbDataFiles) + " fichier : " + str(n+1) + " critere : " + criteria);
+			# if(jvmMode): self.gateway.jvm.System.out.println(str(n) + " - " + dataFiles[n]);
 			trialNumber = trialName.split("\u00b0")[1];
 			system = sessionsProperties[key + "_SYSTEM"];
 			baseTrialsNumber = sessionsProperties[sessionName + baseTrialsNumber_QN];
@@ -78,20 +85,19 @@ class DOCoMETRe(object):
 				append = False;
 				trialsList = createdCategories[criteria];
 
-				self.gateway.jvm.System.out.println(trialsList);
-
-				if isinstance(trialsList, list):
+				if isinstance(trialsList, numpy.ndarray):
 					if int(trialNumber) not in trialsList:
 						append = True;
-				else:
-					if int(trialNumber) != trialsList:
-						append = True;
+				#else:
+				#	if int(trialNumber) != trialsList:
+				#		append = True;
 
 				if append:
 					trialsList = numpy.append(trialsList, int(trialNumber));
+					#if(jvmMode): self.gateway.jvm.System.out.println(ListConverter().convert(trialsList.tolist(), gateway._gateway_client));
 			else:
-
-				trialsList = int(trialNumber);
+				trialsList = numpy.array(int(trialNumber));
+				#if(jvmMode): self.gateway.jvm.System.out.println(trialsList);
 
 			createdCategories[criteria] = trialsList;
 
@@ -112,7 +118,7 @@ class DOCoMETRe(object):
 				data = data[2::2];
 
 			sizeData = len(data);
-			#self.gateway.jvm.System.out.println("sizeData : " + str(sizeData));
+			#if(jvmMode): self.gateway.jvm.System.out.println("sizeData : " + str(sizeData));
 			key = loadName + "." + channelName + ".NbSamples." + trialNumber;
 			experiments[key] = sizeData;
 			key = loadName + "." + channelName + ".EndCut." + trialNumber;
@@ -120,27 +126,65 @@ class DOCoMETRe(object):
 			key = loadName + "." + channelName + ".FrontCut." + trialNumber;
 			experiments[key] = 0;
 			values = experiments[loadName + "." + channelName + ".Values"];
-			#self.gateway.jvm.System.out.println("size values : " + str(len(values[int(trialNumber) - 1])));
+			#if(jvmMode): self.gateway.jvm.System.out.println("size values : " + str(len(values[int(trialNumber) - 1])));
 			values[int(trialNumber) - 1][0:sizeData] = data;
 
 		n = 1;
 		for criteria in createdCategories:
 			values = createdCategories[criteria];
-			values.sort();
 			experiments[loadName + ".Category" + str(n) + ".Criteria"] = criteria;
 			experiments[loadName + ".Category" + str(n) + ".TrialsList"] = values;
-			experiments[loadName + "." + channelName + "." + "isSignal"] = "0";
-			experiments[loadName + "." + channelName + "." + "isCategory"] = "1";
-			experiments[loadName + "." + channelName + "." + "isEvent"] = "0";
+			experiments[loadName + ".Category" + str(n) + ".isSignal"] = "0";
+			experiments[loadName + ".Category" + str(n) + ".isCategory"] = "1";
+			experiments[loadName + ".Category" + str(n) + ".isEvent"] = "0";
 			n = n + 1;
 
 	class Java:
 		implements = ["fr.univamu.ism.docometre.python.PythonEntryPoint"]
 
 if __name__ == "__main__":
+	
+	print("Current working folder : " + os.getcwd());
+	jvmMode = True;
+	
 	experiments = dict();
-	gateway = ClientServer(java_parameters = JavaParameters(), python_parameters = PythonParameters());
-	#gateway = 10
-	docometre = DOCoMETRe(gateway);
-	#docometre.loadData("DOCOMETRE", None, None, None);
-	gateway.entry_point.setPythonEntryPoint(docometre);
+	
+	if(sys.argv[1] == "-jvm"):	
+		
+		gateway = ClientServer(java_parameters = JavaParameters(), python_parameters = PythonParameters());	
+		docometre = DOCoMETRe(gateway);
+		gateway.entry_point.setPythonEntryPoint(docometre);
+		
+	else:
+		print("We are not in JVM mode");
+		jvmMode = False;
+		
+		docometre = DOCoMETRe(None);
+		
+		loadName = "ReachabilityCoriolis.PreTestFull";
+
+		fileHandle = io.open("./scripts/PythonScripts/dataFilesList.txt", "r", encoding="utf-8");		
+		dataFilesList = fileHandle.read();
+		fileHandle.close();
+		
+		fileHandle = io.open("./scripts/PythonScripts/sessionsProperties.txt", "r", encoding="utf-8");		
+		sessionPropertiesString = fileHandle.read();
+		sessionProperties = eval(sessionPropertiesString);
+		fileHandle.close();
+
+		docometre.loadData("DOCOMETRE", loadName, dataFilesList, sessionProperties);
+		
+		print(experiments["ReachabilityCoriolis.PreTestFull.Category1.Criteria"]);
+		print(experiments["ReachabilityCoriolis.PreTestFull.Category1.isSignal"]);
+		print(experiments["ReachabilityCoriolis.PreTestFull.Category1.isCategory"]);
+		print(experiments["ReachabilityCoriolis.PreTestFull.Category1.isEvent"]);
+		print(experiments["ReachabilityCoriolis.PreTestFull.Category1.TrialsList"]);
+		
+		print(21 in experiments["ReachabilityCoriolis.PreTestFull.Category1.TrialsList"]);
+		
+		keys = experiments.keys();
+		print(keys)
+		
+		print(list({k:v for k,v in experiments.items() if re.search("isSignal$", k) and v == "1"}))
+		
+	
