eb06e46b77723d22f7ba97953268051b1e8dffc0
frank buloup <frank.buloup@univ-amu.fr>
2021-02-09 14:24:51 +0100

    Improve Mean function with vectorization (Python, Matlab)
    Signed-off-by: frank buloup <frank.buloup@univ-amu.fr>

--- Docometre/src/fr/univamu/ism/docometre/analyse/functions/Mean.java
+++ Docometre/src/fr/univamu/ism/docometre/analyse/functions/Mean.java
@@ -1,9 +1,6 @@
 package fr.univamu.ism.docometre.analyse.functions;
 
-import java.util.function.Function;
-
 import org.eclipse.jface.viewers.ComboViewer;
-import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.SWT;
@@ -33,8 +30,8 @@ public class Mean extends GenericFunction {
 	public static final String functionFileName = "MEAN.FUN";
 	
 	private static final String inputSignalKey = "inputSignal";
-	private static final String fromKey = "from";
-	private static final String toKey = "to";
+	private static final String fromInputSignalKey = "fromInputSignal";
+	private static final String toInputSignalKey = "toInputSignal";
 	
 	transient private FunctionalBlockConfigurationDialog functionalBlockConfigurationDialog;
 	
@@ -50,7 +47,7 @@ public class Mean extends GenericFunction {
 		Composite container  = (Composite) parent;
 		Composite paramContainer = new Composite(container, SWT.NORMAL);
 		paramContainer.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-		paramContainer.setLayout(new GridLayout(2, false));
+		paramContainer.setLayout(new GridLayout(3, false));
 		
 		this.functionalBlockConfigurationDialog = (FunctionalBlockConfigurationDialog) functionalBlockConfigurationDialog;
 		
@@ -66,16 +63,10 @@ public class Mean extends GenericFunction {
 		ComboViewer inputSignalComboViewer = new ComboViewer(paramContainer, SWT.BORDER);
 		inputSignalComboViewer.getCombo().setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
 		String value  = getProperty(inputSignalKey, "");
+		ChannelsContentProvider channelsContentProvider = new ChannelsContentProvider(true, false, false, false, false, false, false);
 		inputSignalComboViewer.getCombo().setText(value);
-		inputSignalComboViewer.setContentProvider(new ChannelsContentProvider(true, false, false, false, false, false, false));
-		inputSignalComboViewer.setLabelProvider(LabelProvider.createTextProvider(new Function<Object, String>() {
-			@Override
-			public String apply(Object t) {
-				if(!(t instanceof Channel)) return null;
-				Channel channel = (Channel)t;
-				return channel.getFullName();
-			}
-		}));
+		inputSignalComboViewer.setContentProvider(channelsContentProvider);
+		inputSignalComboViewer.setLabelProvider(FunctionsHelper.createTextProvider());
 		inputSignalComboViewer.setInput(SelectedExprimentContributionItem.selectedExperiment);
 		Channel channel = MathEngineFactory.getMathEngine().getChannelFromName(SelectedExprimentContributionItem.selectedExperiment, getProperty(inputSignalKey, ""));
 		if(channel != null) inputSignalComboViewer.setSelection(new StructuredSelection(channel));
@@ -91,6 +82,7 @@ public class Mean extends GenericFunction {
 				getTransientProperties().put(inputSignalKey, inputSignalComboViewer.getCombo().getText());
 			}
 		});
+		FunctionsHelper.addChannelsSelectionButton(paramContainer, "...", inputSignalComboViewer, channelsContentProvider);
 		
 		// From
 		Label fromLabel = new Label(paramContainer, SWT.NONE);
@@ -99,19 +91,13 @@ public class Mean extends GenericFunction {
 		
 		ComboViewer fromComboViewer = new ComboViewer(paramContainer, SWT.BORDER);
 		fromComboViewer.getCombo().setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
-		value  = getProperty(fromKey, "");
+		value  = getProperty(fromInputSignalKey, "");
 		fromComboViewer.getCombo().setText(value);
-		fromComboViewer.setContentProvider(new ChannelsContentProvider(false, false, false, true, false, true, true));
-		fromComboViewer.setLabelProvider(LabelProvider.createTextProvider(new Function<Object, String>() {
-			@Override
-			public String apply(Object t) {
-				if(!(t instanceof Channel)) return null;
-				Channel channel = (Channel)t;
-				return channel.getFullName();
-			}
-		}));
+		ChannelsContentProvider channelsContentProviderFromTo = new ChannelsContentProvider(true, false, false, true, false, true, true);
+		fromComboViewer.setContentProvider(channelsContentProviderFromTo);
+		fromComboViewer.setLabelProvider(FunctionsHelper.createTextProvider());
 		fromComboViewer.setInput(SelectedExprimentContributionItem.selectedExperiment);
-		channel = MathEngineFactory.getMathEngine().getChannelFromName(SelectedExprimentContributionItem.selectedExperiment, getProperty(fromKey, ""));
+		channel = MathEngineFactory.getMathEngine().getChannelFromName(SelectedExprimentContributionItem.selectedExperiment, getProperty(fromInputSignalKey, ""));
 		if(channel != null) fromComboViewer.setSelection(new StructuredSelection(channel));
 		else {
 			String message = NLS.bind(DocometreMessages.ImpossibleToFindChannelTitle, value);
@@ -122,9 +108,10 @@ public class Mean extends GenericFunction {
 			@Override
 			public void modifyText(ModifyEvent e) {
 				Mean.this.functionalBlockConfigurationDialog.setErrorMessage(null);
-				getTransientProperties().put(fromKey, fromComboViewer.getCombo().getText());
+				getTransientProperties().put(fromInputSignalKey, fromComboViewer.getCombo().getText());
 			}
 		});
+		FunctionsHelper.addChannelsSelectionButton(paramContainer, "...", fromComboViewer, channelsContentProviderFromTo);
 		
 		// To
 		Label toLabel = new Label(paramContainer, SWT.NONE);
@@ -133,19 +120,12 @@ public class Mean extends GenericFunction {
 		
 		ComboViewer toComboViewer = new ComboViewer(paramContainer, SWT.BORDER);
 		toComboViewer.getCombo().setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
-		value  = getProperty(toKey, "");
+		value  = getProperty(toInputSignalKey, "");
 		toComboViewer.getCombo().setText(value);
 		toComboViewer.setContentProvider(new ChannelsContentProvider(false, false, false, true, false, true, true));
-		toComboViewer.setLabelProvider(LabelProvider.createTextProvider(new Function<Object, String>() {
-			@Override
-			public String apply(Object t) {
-				if(!(t instanceof Channel)) return null;
-				Channel channel = (Channel)t;
-				return channel.getFullName();
-			}
-		}));
+		toComboViewer.setLabelProvider(FunctionsHelper.createTextProvider());
 		toComboViewer.setInput(SelectedExprimentContributionItem.selectedExperiment);
-		channel = MathEngineFactory.getMathEngine().getChannelFromName(SelectedExprimentContributionItem.selectedExperiment, getProperty(toKey, ""));
+		channel = MathEngineFactory.getMathEngine().getChannelFromName(SelectedExprimentContributionItem.selectedExperiment, getProperty(toInputSignalKey, ""));
 		if(channel != null) toComboViewer.setSelection(new StructuredSelection(channel));
 		else {
 			String message = NLS.bind(DocometreMessages.ImpossibleToFindChannelTitle, value);
@@ -156,11 +136,12 @@ public class Mean extends GenericFunction {
 			@Override
 			public void modifyText(ModifyEvent e) {
 				Mean.this.functionalBlockConfigurationDialog.setErrorMessage(null);
-				getTransientProperties().put(toKey, toComboViewer.getCombo().getText());
+				getTransientProperties().put(toInputSignalKey, toComboViewer.getCombo().getText());
 			}
 		});
+		FunctionsHelper.addChannelsSelectionButton(paramContainer, "...", toComboViewer, channelsContentProviderFromTo);
 		
-		addCommentField(paramContainer, 1, context);
+		addCommentField(paramContainer, 2, context);
 		
 		return paramContainer;
 	}
@@ -170,7 +151,9 @@ public class Mean extends GenericFunction {
 		String code = FunctionFactory.getProperty(context, functionFileName, FUNCTION_CODE);
 		
 		String inputSignal = getProperty(inputSignalKey, "");
-		code = code.replaceAll(inputSignalKey, inputSignal);
+		String fromInputSignal = getProperty(fromInputSignalKey, "");
+		String toInputSignal = getProperty(toInputSignalKey, "");
+		code = code.replaceAll(inputSignalKey, inputSignal).replaceAll(fromInputSignalKey, fromInputSignal).replaceAll(toInputSignalKey, toInputSignal);
 		
 		return code + "\n";
 	}
