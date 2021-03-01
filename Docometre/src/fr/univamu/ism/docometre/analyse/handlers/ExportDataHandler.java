a275433edd379644df7c64857b1d59f5026c833c
frank buloup <frank.buloup@univ-amu.fr>
2021-03-01 13:00:44 +0100

    Start Export data menu, toolbar and view items
    Signed-off-by: frank buloup <frank.buloup@univ-amu.fr>

---
+++ Docometre/src/fr/univamu/ism/docometre/analyse/handlers/ExportDataHandler.java
@@ -0,0 +1,16 @@
+package fr.univamu.ism.docometre.analyse.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class ExportDataHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		return null;
+	}
+
+	
+
+}
