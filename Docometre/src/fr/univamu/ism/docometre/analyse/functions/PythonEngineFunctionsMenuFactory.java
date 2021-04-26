f9b995a90e9f72537c952736dec68c8d6e7d2c7c
frank buloup <frank.buloup@univ-amu.fr>
2021-04-26 17:16:01 +0200

    Add Maximum function (Python)
    Signed-off-by: frank buloup <frank.buloup@univ-amu.fr>

--- Docometre/src/fr/univamu/ism/docometre/analyse/functions/PythonEngineFunctionsMenuFactory.java
+++ Docometre/src/fr/univamu/ism/docometre/analyse/functions/PythonEngineFunctionsMenuFactory.java
@@ -72,8 +72,8 @@ public class PythonEngineFunctionsMenuFactory {
 	public static String[] FilteringFunctionsFiles = new String[] {ButterworthLowPass.functionFileName, ButterworthHighPass.functionFileName};
 	public static String[] FilteringFunctionsClasses = new String[] {ButterworthLowPass.class.getName(), ButterworthHighPass.class.getName()};
 	
-	public static String[] MarkersFunctionsFiles = new String[] {TimeMarker.functionFileName};
-	public static String[] MarkersFunctionsClasses = new String[] {TimeMarker.class.getName()};
+	public static String[] MarkersFunctionsFiles = new String[] {TimeMarker.functionFileName, Maximum.functionFileName};
+	public static String[] MarkersFunctionsClasses = new String[] {TimeMarker.class.getName(), Maximum.class.getName()};
 	
 	public static String[] FeaturesFunctionsFiles = new String[] {Mean.functionFileName};
 	public static String[] FeaturesFunctionsClasses = new String[] {Mean.class.getName()};
