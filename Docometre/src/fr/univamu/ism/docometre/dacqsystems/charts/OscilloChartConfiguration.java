58b7c2941d298154d681cf6cd007ded9adaeeefc
frank buloup <frank.buloup@univ-amu.fr>
2021-05-02 16:00:03 +0200

    oscilloCurveConfiguration DISPLAY_CURRENT_VALUES prop. taken in account
    Signed-off-by: frank buloup <frank.buloup@univ-amu.fr>

--- Docometre/src/fr/univamu/ism/docometre/dacqsystems/charts/OscilloChartConfiguration.java
+++ Docometre/src/fr/univamu/ism/docometre/dacqsystems/charts/OscilloChartConfiguration.java
@@ -244,7 +244,7 @@ public class OscilloChartConfiguration extends ChartConfiguration {
 			int serieStyle = CurveConfigurationProperties.getStyle(oscilloCurveConfiguration);
 			int serieWidth = Integer.parseInt(oscilloCurveConfiguration.getProperty(CurveConfigurationProperties.WIDTH));
 			RTSWTOscilloSerie oscilloSerie = oscilloChart.createSerie(serieID, serieColor, serieStyle, serieWidth);
-			value = getProperty(OscilloCurveConfigurationProperties.DISPLAY_CURRENT_VALUES);
+			value = oscilloCurveConfiguration.getProperty(OscilloCurveConfigurationProperties.DISPLAY_CURRENT_VALUES);
 			boolean displayCurrentValues = Boolean.parseBoolean(value);
 			displayCurrentValues = displayCurrentValuesChart || displayCurrentValues;
 			oscilloSerie.setShowCurrentValue(displayCurrentValues);
