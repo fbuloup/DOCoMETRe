798b27b4404faa28697ad5842ffef47d187c39a3
frank buloup <frank.buloup@univ-amu.fr>
2021-09-09 16:11:54 +0200

    Add Butt. high pass, front & end cut to math. engine
    Signed-off-by: frank buloup <frank.buloup@univ-amu.fr>

--- Docometre/src/fr/univamu/ism/docometre/analyse/functions/MatlabEngineFunctionsMenuFactory.java
+++ Docometre/src/fr/univamu/ism/docometre/analyse/functions/MatlabEngineFunctionsMenuFactory.java
@@ -57,24 +57,51 @@ public class MatlabEngineFunctionsMenuFactory {
 	
 	private static String SEPARATOR = "SEPARATOR";
 	private static String CUSTOMER_FUNCTIONS_MENU = "CUSTOMER_FUNCTIONS_MENU";
+	private static String SUBMENU_SIGNALS = "SUBMENU_SIGNALS";
+	private static String SUBMENU_MARKERS = "SUBMENU_MARKERS";
+	private static String SUBMENU_FEATURES = "SUBMENU_FEATURES";
+	private static String SUBMENU_EVENTS = "SUBMENU_EVENTS";
+	private static String SUBMENU_FILTERING = "SUBMENU_FILTERING";
 	
-	public static String[] MatlabEngineFunctionsFiles = new String[] {ButterworthLowPass.functionFileName, 
-		    														SEPARATOR,
-																	Mean.functionFileName,
-																	/*DigitalInputFunction.functionFileName,
-																	SEPARATOR,
-																    ExpressionFunction.functionFileName,
-																    SEPARATOR,
-																    TerminateProcessFunction.functionFileName*/};
+	public static String[] MatlabEngineFunctionsFiles = new String[] {SUBMENU_SIGNALS, SUBMENU_MARKERS, SUBMENU_FEATURES, SUBMENU_EVENTS};
+	public static String[] MatlabEngineFunctionsClasses = new String[] {null, null, null, null};
 	
-	public static String[] MatlabEngineFunctionsClasses = new String[] {ButterworthLowPass.class.getName(),
-																	  null,
-																	  Mean.class.getName(),
-																	  /*DigitalInputFunction.class.getName(),
-																	  null,
-																	  ExpressionFunction.class.getName(),
-																	  null,
-																	  TerminateProcessFunction.class.getName()*/};
+	public static String[] SignalsFunctionsFiles = new String[] {SUBMENU_FILTERING, SEPARATOR, FrontCut.functionFileName, EndCut.functionFileName, FrontCutFromMarker.functionFileName ,EndCutFromMarker.functionFileName, SEPARATOR, 
+																	MotionDistance.functionFileName, MotionDirection.functionFileName, SEPARATOR, Derivative.functionFileName};
+	public static String[] SignalsFunctionsClasses = new String[] {null, null, FrontCut.class.getName(), EndCut.class.getName(), FrontCutFromMarker.class.getName() ,EndCutFromMarker.class.getName(), null, 
+																	MotionDistance.class.getName(), MotionDirection.class.getName(), null, Derivative.class.getName()};
+	
+	public static String[] FilteringFunctionsFiles = new String[] {ButterworthLowPass.functionFileName, ButterworthHighPass.functionFileName};
+	public static String[] FilteringFunctionsClasses = new String[] {ButterworthLowPass.class.getName(), ButterworthHighPass.class.getName()};
+	
+	public static String[] MarkersFunctionsFiles = new String[] {TimeMarker.functionFileName, Maximum.functionFileName, Minimum.functionFileName, FindAmplitudeBackward.functionFileName, FindAmplitudeForward.functionFileName,
+																	TransferMarkersGroup.functionFileName};
+	public static String[] MarkersFunctionsClasses = new String[] {TimeMarker.class.getName(), Maximum.class.getName(), Minimum.class.getName(), FindAmplitudeBackward.class.getName(), FindAmplitudeForward.class.getName(),
+																	TransferMarkersGroup.class.getName()};
+	
+	public static String[] FeaturesFunctionsFiles = new String[] {Mean.functionFileName};
+	public static String[] FeaturesFunctionsClasses = new String[] {Mean.class.getName()};
+	
+	public static String[] EventsFunctionsFiles = new String[] {};
+	public static String[] EventsFunctionsClasses = new String[] {};
+	
+//	public static String[] MatlabEngineFunctionsFiles = new String[] {ButterworthLowPass.functionFileName, 
+//		    														SEPARATOR,
+//																	Mean.functionFileName,
+//																	/*DigitalInputFunction.functionFileName,
+//																	SEPARATOR,
+//																    ExpressionFunction.functionFileName,
+//																    SEPARATOR,
+//																    TerminateProcessFunction.functionFileName*/};
+//	
+//	public static String[] MatlabEngineFunctionsClasses = new String[] {ButterworthLowPass.class.getName(),
+//																	  null,
+//																	  Mean.class.getName(),
+//																	  /*DigitalInputFunction.class.getName(),
+//																	  null,
+//																	  ExpressionFunction.class.getName(),
+//																	  null,
+//																	  TerminateProcessFunction.class.getName()*/};
 	
 	public static void populateMenu(AbstractScriptSegmentEditor scriptSegmentEditor, BlockEditPart blockEditPart,
 			IMenuManager functionMenuManager) {
@@ -85,6 +112,22 @@ public class MatlabEngineFunctionsMenuFactory {
 		for (String functionFilePath : MatlabEngineFunctionsFiles) {
 			if (functionFilePath.equals(SEPARATOR)) {
 				functionMenuManager.add(new Separator());
+			}  else if(functionFilePath.equals(SUBMENU_SIGNALS)) {
+				MenuManager subMenuManager = new MenuManager(FunctionsMessages.Signals, SUBMENU_SIGNALS);
+				populateSubMenu(subMenuManager, context, scriptSegmentEditor, blockEditPart);
+				functionMenuManager.add(subMenuManager);
+			} else if(functionFilePath.equals(SUBMENU_MARKERS)) {
+				MenuManager subMenuManager = new MenuManager(FunctionsMessages.Markers, SUBMENU_MARKERS);
+				populateSubMenu(subMenuManager, context, scriptSegmentEditor, blockEditPart);
+				functionMenuManager.add(subMenuManager);
+			} else if(functionFilePath.equals(SUBMENU_FEATURES)) {
+				MenuManager subMenuManager = new MenuManager(FunctionsMessages.Features, SUBMENU_FEATURES);
+				populateSubMenu(subMenuManager, context, scriptSegmentEditor, blockEditPart);
+				functionMenuManager.add(subMenuManager);
+			} else if(functionFilePath.equals(SUBMENU_EVENTS)) {
+				MenuManager subMenuManager = new MenuManager(FunctionsMessages.Events, SUBMENU_EVENTS);
+				populateSubMenu(subMenuManager, context, scriptSegmentEditor, blockEditPart);
+				functionMenuManager.add(subMenuManager);
 			} else {
 				String menuTitle = FunctionFactory.getProperty(context, functionFilePath, FunctionFactory.MENU_TITLE);
 				String menuTooltip = FunctionFactory.getProperty(context, functionFilePath, FunctionFactory.DESCRIPTION);
@@ -110,6 +153,47 @@ public class MatlabEngineFunctionsMenuFactory {
 				}
 			}
 			functionMenuManager.add(customerFunctionsMenuManager);
+		}
+	}
+	
+	private static void populateSubMenu(MenuManager subMenuManager, Object context, AbstractScriptSegmentEditor scriptSegmentEditor, BlockEditPart blockEditPart) {
+		if(subMenuManager.getId().equals(SUBMENU_SIGNALS)) {
+			MenuManager subSubMenuManager = new MenuManager(FunctionsMessages.Filtering, SUBMENU_FILTERING);
+			populateSubMenu(subSubMenuManager, context, scriptSegmentEditor, blockEditPart);
+			subMenuManager.add(subSubMenuManager);
+			createSubmenuActions(subMenuManager, SignalsFunctionsFiles, SignalsFunctionsClasses, context, scriptSegmentEditor, blockEditPart);
+		}
+		if(subMenuManager.getId().equals(SUBMENU_FILTERING)) {
+			createSubmenuActions(subMenuManager, FilteringFunctionsFiles, FilteringFunctionsClasses, context, scriptSegmentEditor, blockEditPart);
+		}
+		if(subMenuManager.getId().equals(SUBMENU_MARKERS)) {
+			createSubmenuActions(subMenuManager, MarkersFunctionsFiles, MarkersFunctionsClasses, context, scriptSegmentEditor, blockEditPart);
+		}
+
+		if(subMenuManager.getId().equals(SUBMENU_FEATURES)) {
+			createSubmenuActions(subMenuManager, FeaturesFunctionsFiles, FeaturesFunctionsClasses, context, scriptSegmentEditor, blockEditPart);
+		}
+		if(subMenuManager.getId().equals(SUBMENU_EVENTS)) {
+			createSubmenuActions(subMenuManager, EventsFunctionsFiles, EventsFunctionsClasses, context, scriptSegmentEditor, blockEditPart);
+		}
+	}
+	
+	private static void createSubmenuActions(MenuManager menuManager, String[] files, String[] classes, Object context, AbstractScriptSegmentEditor scriptSegmentEditor, BlockEditPart blockEditPart) {
+		int i = 0;
+		for (String file : files) {
+			if (file.equals(SEPARATOR)) {
+				menuManager.add(new Separator());
+			}  else {
+				if(classes[i] != null) {
+					String menuTitle = FunctionFactory.getProperty(context, file, FunctionFactory.MENU_TITLE);
+					String menuTooltip = FunctionFactory.getProperty(context, file, FunctionFactory.DESCRIPTION);
+					if(menuTitle != null && menuTooltip != null) {
+						AssignFunctionAction assignFunctionAction = new AssignFunctionAction(scriptSegmentEditor, blockEditPart, menuTitle, menuTooltip, classes[i]);
+						menuManager.add(assignFunctionAction);
+					}
+				}
+			}
+			i++;
 		}
 	}
 
