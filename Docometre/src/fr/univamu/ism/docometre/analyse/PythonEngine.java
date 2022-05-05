9a61d0edd5404c04ee63f5340010c6931c216bd2
frank buloup <frank.buloup@univ-amu.fr>
2022-05-05 10:59:33 +0200

    Add find peaks functions (Python only)
    Signed-off-by: frank buloup <frank.buloup@univ-amu.fr>

--- Docometre/src/fr/univamu/ism/docometre/analyse/PythonEngine.java
+++ Docometre/src/fr/univamu/ism/docometre/analyse/PythonEngine.java
@@ -234,6 +234,7 @@ public class PythonEngine implements MathEngine {
 				boolean isOptitrack = Analyse.isOptitrack(dataFilesList.split(";"), (IContainer) subject);
 				if(isOptitrack) {
 					// If all data files are OPTITRACK_TYPE_1
+					System.out.println("OPTITRACK_TYPE_1 : " + loadName + " : " + dataFilesList);
 					pythonController.getPythonEntryPoint().loadData("OPTITRACK_TYPE_1", loadName, dataFilesList, null);
 				} else {
 					Map<String, String> sessionsProperties = Analyse.getSessionsInformations(subject);
