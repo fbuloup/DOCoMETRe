69252d608241419904af0ddce1bb833a8a3cdc2c
frank buloup <frank.buloup@univ-amu.fr>
2024-06-09 20:03:56 +0200

    Reset/Continue

--- JZY3D/src/org/jzy3d/plot3d/rendering/lights/LightModel.java
+++
@@ -1,5 +0,0 @@
-package org.jzy3d.plot3d.rendering.lights;
-
-public enum LightModel {
-  LIGHT_MODEL_TWO_SIDE, LIGHT_MODEL_LOCAL_VIEWER
-}
--- org.eclipse.swtchart.extensions/src/org/eclipse/swtchart/extensions/charts/ChartPropertiesListener.java
+++
@@ -1,5 +0,0 @@
-package org.eclipse.swtchart.extensions.charts;
-
-public interface ChartPropertiesListener {
-	void updateChartProperties();
-}
