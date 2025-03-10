b141c6f89b893d907499446327f6730814e7f076
frank buloup <frank.buloup@univ-amu.fr>
2025-03-10 14:02:02 +0100

    Add AR_DEFAULT choice and change regular expression
    Signed-off-by: frank buloup <frank.buloup@univ-amu.fr>

--- Docometre/src/fr/univamu/ism/docometre/dacqsystems/arduinouno/ArduinoUnoAnInModuleProperties.java
+++ Docometre/src/fr/univamu/ism/docometre/dacqsystems/arduinouno/ArduinoUnoAnInModuleProperties.java
@@ -46,11 +46,12 @@ import fr.univamu.ism.docometre.dacqsystems.Property;
 
 public final class ArduinoUnoAnInModuleProperties extends Property {
 	
-	public static final ArduinoUnoAnInModuleProperties ANALOG_REFERENCE = new ArduinoUnoAnInModuleProperties("ArduinoUnoAnInModuleProperties.ANALOG_REFERENCE", ArduinoUnoMessages.AnalogReference_Label, ArduinoUnoMessages.AnalogReference_Tooltip, "\"^(DEFAULT|INTERNAL|EXTERNAL)$\"", "DEFAULT:INTERNAL:EXTERNAL");
+	public static final ArduinoUnoAnInModuleProperties ANALOG_REFERENCE = new ArduinoUnoAnInModuleProperties("ArduinoUnoAnInModuleProperties.ANALOG_REFERENCE", ArduinoUnoMessages.AnalogReference_Label, ArduinoUnoMessages.AnalogReference_Tooltip, "^(DEFAULT|INTERNAL|EXTERNAL|AR_DEFAULT)$", "DEFAULT:INTERNAL:EXTERNAL:AR_DEFAULT");
 	
-	public static String DEFAULT = "DEFAULT";   
+	public static String DEFAULT = "DEFAULT";
 	public static String INTERNAL = "INTERNAL";
 	public static String EXTERNAL = "EXTERNAL";
+	public static String AR_DEFAULT = "AR_DEFAULT";
 	
 	public static void populateProperties(Module module){
 		module.setProperty(ANALOG_REFERENCE, DEFAULT); 
