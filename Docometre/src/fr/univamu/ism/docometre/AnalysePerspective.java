88c88891aa90d816f0e70729b5554d2f6a721e83
fbuloup <frank.buloup@univ-amu.fr>
2026-04-01 22:57:30 +0200

    Use default console in Analyse perspective
    Signed-off-by: fbuloup <frank.buloup@univ-amu.fr>

--- Docometre/src/fr/univamu/ism/docometre/AnalysePerspective.java
+++ Docometre/src/fr/univamu/ism/docometre/AnalysePerspective.java
@@ -44,6 +44,7 @@ package fr.univamu.ism.docometre;
 import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
+import org.eclipse.ui.console.IConsoleConstants;
 
 import fr.univamu.ism.docometre.analyse.views.MathEngineCommandView;
 import fr.univamu.ism.docometre.analyse.views.SubjectsView;
@@ -58,7 +59,8 @@ public class AnalysePerspective implements IPerspectiveFactory {
 		layout.addView(SubjectsView.ID, IPageLayout.LEFT, .25f, layout.getEditorArea());
 //		layout.getViewLayout(ExperimentsView.ID).setCloseable(false);
 //		layout.addView(DescriptionView.ID, IPageLayout.BOTTOM, .75f, ExperimentsView.ID);
-		layout.addView(MessagesView.ID, IPageLayout.BOTTOM, .75f, layout.getEditorArea());
+//		layout.addView(MessagesView.ID, IPageLayout.BOTTOM, .75f, layout.getEditorArea());
+		layout.addView(IConsoleConstants.ID_CONSOLE_VIEW, IPageLayout.BOTTOM, .75f, layout.getEditorArea());
 		layout.addView(MathEngineCommandView.ID, IPageLayout.RIGHT, .5f, MessagesView.ID);
 		if(System.getProperty("Sleak") != null) {
 			IFolderLayout folder = layout.createFolder("Sleak folder",  IPageLayout.RIGHT, .5f, layout.getEditorArea());
